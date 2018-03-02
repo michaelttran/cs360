e9a13724813d1d4f383c7190b9500e225b3aac53
michaeltranDT <tktran5@usfca.edu>
2018-03-02 13:18:23 -0800

    Added new file for forced layout

--- assignment3/force.js
+++ assignment3/force.js
@@ -5,7 +5,6 @@ var increAmount; // How much to increase each tick on the axis by
 var xAxisMid = []; // To value of the middle coordinate between two axis points: Labeling of node on axes
 var yAxisMid = [];
 var matrxCntr = [];
-var node1, node2;
 
 function preload() {
 	// Variable to change what .edges file to read
@@ -140,76 +139,6 @@ function draw() {
 	background(255);
   // noStroke();
 
-	// Lines for the axis- goes left, up, right, down
-    line(75, 65, 75, 1565);
-    line(75, 65, 1575, 65);
-    line(1575, 65, 1575, 1565);
-    line(75, 1565, 1575, 1565);
-    
-    // Lines for the y axis/columns
-    for(var i = 75; i <= 1575; i += increAmount) {
-    	line(i, 1565, i, 65);
-    }
-    
-    // Lines for the x axis/rows
-    for(var i = 65; i <= 1565; i += increAmount) {
-    	line(75, i, 1575, i);
-    }
 
-    // Printing each node on the x axis
- 	  var xCount = 0;
-    for(var i = 0; i < nodeArr.length; i++) {
-    	text(nodeArr[i], xAxisMid[xCount], 40, 100, 100); 
-    	xCount++;
-    }
-
-    // Printing each node on the y axis
-  	var yCount = 0;
-    for(var i = 0; i < nodeArr.length; i++) {
-    	text(nodeArr[i], 50, yAxisMid[yCount] - 10, 100, 100);
-    	yCount++;
-    }
-
-    // Label of the x axis
-   	var y_cat = "Nodes";
-  	text(y_cat, 30, 10, 1000, 1000);
-
-  	// Label of the y axis
-  	var x_cat = "Nodes";
-  	text(x_cat, 5, 60, 1000, 1000);
-
-    // Title
-    var title = "Matrix representation of friends";
-    textSize(32);
-    text(title, 700, 0, 1000, 1000);    
-
-    textSize(12);
-
-  // Filled points
-  for(var i = 0; i < table.getRowCount(); i++) {
-    node1 = table.getNum(i, 1);
-    node2 = table.getNum(i, 0);
-    var position = nodeArr.indexOf(node1) * nodeArr.indexOf(node2);
-    var size = map(90, 0, 99, 0, increAmount);
-
-    fill(255, 204, 0);
-
-    ellipse(matrxCntr[position].x_val, matrxCntr[position].y_val, size, size);
-    matrxCntr[position].filled = true;
-  }
-  fill(0);
-
-
-	// Implement highlight feature
-  for(var i = 0; i < matrxCntr.length; i++) {
-    var distance = dist(matrxCntr[i].x_val, matrxCntr[i].y_val, mouseX, mouseY);
-    if( distance < 10) {
-      if(matrxCntr[i].filled == true) {
-        fill(0);
-        ellipse(matrxCntr[i].x_val, matrxCntr[i].y_val, size, size); 
-      }
-    }
-  }
-  fill(0);
 
 }
